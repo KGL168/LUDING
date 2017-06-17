4132fa3d8a95c0a024a9fda24c609b81a9a27194
KGL168 <779351046@qq.com>
2017-06-17 14:54:01 +0800

    1

---
+++ LUDING/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  LUDING
+//
+//  Created by HotFire on 2017/6/17.
+//  Copyright © 2017年 HotFire. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    //天地会
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
